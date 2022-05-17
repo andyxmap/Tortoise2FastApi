1cdedf8c01db5e0885558870d2d618baefc71502
AndyxDeveloper <78869806+andyxmap@users.noreply.github.com>
2022-05-17 13:51:04 -0400

    Delete user.py

--- src/crud/user.py
+++
@@ -1,9 +0,0 @@
-from src.models.user import Users, User_Pydantic, UserIn_Pydantic
-from src.crud.base import CRUD
-
-
-class User(CRUD[Users, User_Pydantic, User_Pydantic, UserIn_Pydantic]):
-    pass
-
-
-user = User(Users)
