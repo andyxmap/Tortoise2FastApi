a75b295c57253d8a606b35fb1639b0f0a1257923
AndyxDeveloper <78869806+andyxmap@users.noreply.github.com>
2022-05-19 11:25:48 -0400

    Developer model example changes

--- src/models/developer.py
+++ src/models/developer.py
@@ -14,14 +14,7 @@ class Developer(Base):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=255)
     git_repository = fields.TextField(validators=[RegexValidator('(?:https?://)?(?:www[.])?github[.]com/[\w-]+/?',re.I)])
+    pro: fields.ReverseRelation[Project]
 
-    projects:fields.ReverseRelation[Project]
-
-    class PydanticMeta:
-        allow_cycles = True
-        max_recursion = 4
-
-
-
-Developer_Pydantic = pydantic_model_creator(Developer,name="Developer")
-Developer_PydanticIN = pydantic_model_creator(Developer,name="Developer", exclude_readonly=True)
+Developer_Pydantic = pydantic_model_creator(Developer,name="Developers")
+Developer_PydanticIN = pydantic_model_creator(Developer,name="Developers", exclude_readonly=True)
